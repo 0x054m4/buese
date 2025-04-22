be8f86f21cbda504df4d802a257d99ce3371417a
1-cret <omayr242994@bue.edu.eg>
2025-04-22 18:52:41 +0200

    .

--- UniversityLibrarySystem/src/StudentLogin.java
+++ UniversityLibrarySystem/src/StudentLogin.java
@@ -25,17 +25,27 @@ public class StudentLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("D:\\Omayr\\BUE\\Y1\\S2\\Software Engineering 1\\Project\\Final Phase\\Code\\buese\\buese\\UniversityLibrarySystem\\src\\icons\\book-read-library-study-line-icon-illustration-logo-template-suitable-for-many-purposes-free-vector.jpg")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(131, 131, 131)
+                .addComponent(jLabel1)
+                .addContainerGap(69, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(93, 93, 93)
+                .addComponent(jLabel1)
+                .addContainerGap(107, Short.MAX_VALUE))
         );
 
         pack();
@@ -77,5 +87,6 @@ public class StudentLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
